17f9b7913a4fc3ab276c800c32c607b3d465f370
WesChege <wesleychege@gmail.com>
2020-05-05 23:57:59 +0200

    replaced the fetch API with axios

--- dist/js/products.js
+++ dist/js/products.js
@@ -3,8 +3,9 @@
 class Products {
 	async getProducts() {
 		try {
-			let result = await fetch("/dist/assets/products.json");
-			let data = await result.json();
+			let result = await axios.get("/dist/assets/products.json");
+			//console.log();
+			let data = await JSON.parse(result.request.response);
 			let products = data.items;
 			products = products.map((item) => {
 				const { title, price } = item.fields;
