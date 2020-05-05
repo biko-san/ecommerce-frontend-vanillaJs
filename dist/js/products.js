6ad56676844a3435169b58a180085905137f2cff
WesChege <wesleychege@gmail.com>
2020-05-06 00:10:15 +0200

    revert to fetch API

--- dist/js/products.js
+++ dist/js/products.js
@@ -3,9 +3,13 @@
 class Products {
 	async getProducts() {
 		try {
-			let result = await axios.get("/dist/assets/products.json");
-			//console.log();
-			let data = await JSON.parse(result.request.response);
+			//axios implementation
+			// let result = await axios.get("/dist/assets/products.json");
+			// let data = await JSON.parse(result.request.response);
+
+			//fetch API implemetation
+			let result = await fetch("/dist/assets/products.json");
+			let data = await result.json();
 			let products = data.items;
 			products = products.map((item) => {
 				const { title, price } = item.fields;
